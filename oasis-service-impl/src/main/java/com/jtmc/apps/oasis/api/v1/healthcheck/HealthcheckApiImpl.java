d89a8b57d4d5850ac7b3b5458cf57e9ec2ba9b8a
JavierTMezaC <javiermeza.cazarez@gmail.com>
2021-10-17 21:05:59 -0700

    removed unused variable

--- oasis-service-impl/src/main/java/com/jtmc/apps/oasis/api/v1/healthcheck/HealthcheckApiImpl.java
+++ oasis-service-impl/src/main/java/com/jtmc/apps/oasis/api/v1/healthcheck/HealthcheckApiImpl.java
@@ -7,11 +7,6 @@ import javax.ws.rs.core.Response;
 
 public class HealthcheckApiImpl implements HealthcheckApi {
 
-    @Inject
-    @Named("key")
-    private String secretKey;
-
-
     @Override
     public Response getStatus() {
         return Response.ok().entity("all chido compa!").build();
